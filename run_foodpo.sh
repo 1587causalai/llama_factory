64e0ed51662b86c3675fc53b9760c9b3f07b5f7c
1587causalai <zj3712@gmail.com>
2025-03-06 15:18:14 +0800

    移除旧的运行脚本

--- run_foodpo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# 设置wandb项目和运行名称
-export WANDB_PROJECT="foodpo"
-export WANDB_NAME="qwen1.5-0.5b-foodpo-test"
-
-# 运行FooDPO训练
-python dpo_baseline/run_foodpo_detailed.py examples/train_lora/qwen1_5_0_5b_lora_foodpo.yaml 
